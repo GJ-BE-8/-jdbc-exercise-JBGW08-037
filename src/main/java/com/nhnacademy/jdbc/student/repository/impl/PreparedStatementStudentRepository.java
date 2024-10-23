786d2dd6cd5090f0f159a81990b3aac6aa31a5e7
Janghyunsung2 <tlswlfk@gmail.com>
2024-10-23 13:55:27 +0900

    save

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/PreparedStatementStudentRepository.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/PreparedStatementStudentRepository.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.jdbc.student.repository.impl;
 
 import com.nhnacademy.jdbc.student.domain.Student;
+import com.nhnacademy.jdbc.student.domain.Student.GENDER;
 import com.nhnacademy.jdbc.student.repository.StudentRepository;
 import com.nhnacademy.jdbc.util.DbUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -14,27 +15,82 @@ public class PreparedStatementStudentRepository implements StudentRepository {
     @Override
     public int save(Student student){
         //todo#1 학생 등록
+        String sql =  String.format("insert jdbc_students(id, name, gender, age) valuse(%s, %s, %s, %d)", student.getId(), student.getName(), student.getGender(), student.getAge());
 
-        return 0;
+        log.debug("sql:{}",sql);
+
+        try(Connection connection = DbUtils.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+//            ResultSet rs = ps.executeQuery();
+        ) {
+            int result = ps.executeUpdate();
+            log.debug("result:{}",result);
+
+            return result;
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public Optional<Student> findById(String id){
         //todo#2 학생 조회
-        return Optional.empty();
+        String sql = String.format("select * from jdbc_students where id = %s", id);
+
+        log.debug("sql:{}",sql);
+
+        try(Connection connection = DbUtils.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+        ) {
+            Student student = new Student(rs.getString("id"),rs.getString("name"),
+                (GENDER) rs.getObject("gender"), rs.getInt("age"));
+
+            log.debug("student:{}",student);
+
+            return Optional.of(student);
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public int update(Student student){
         //todo#3 학생 수정 , name 수정
+        String sql = String.format("update jdbc_students set name = %s where id = %s", student.getName(), student.getId());
+        log.debug("sql:{}",sql);
 
-        return 0;
+        try(Connection connection = DbUtils.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+            int result = ps.executeUpdate();
+            log.debug("result:{}",result);
+
+            return result;
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public int deleteById(String id){
         //todo#4 학생 삭제
-        return 0;
+        String sql = String.format("delete from jdbc_students where id = %s", id);
+        log.debug("sql:{}",sql);
+
+        try(Connection connection = DbUtils.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+            int result = ps.executeUpdate();
+            log.debug("result:{}",result);
+            return result;
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+
     }
 
 }
